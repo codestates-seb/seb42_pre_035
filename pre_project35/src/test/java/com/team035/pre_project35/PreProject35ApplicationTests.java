fd4f12b5df1c8cebc6cedcf154fbda95f8946bcc
leegri <bestest0415@gmail.com>
2023-02-15 17:27:07 +0900

    first

---
+++ pre_project35/src/test/java/com/team035/pre_project35/PreProject35ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team035.pre_project35;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreProject35ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
