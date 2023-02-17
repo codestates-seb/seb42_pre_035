1dead2eb77466ead631a847cbe33dc29716bc134
CHOISANAE <tksdo96@gmail.com>
2023-02-17 19:31:22 +0900

    navBar

---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import LeftNav from '../components/LeftNav/LeftNav';
+import RightNav from '../components/RightNav/RightNav';
+import './Home.css';
+function Home() {
+  return (
+    <div className="h">
+      <LeftNav />
+      <RightNav />
+    </div>
+  );
+}
+export default Home;
