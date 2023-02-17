c8317488bc5edbb37fd7b22825be938d36f8cada
Im Sung Eun <se2id@naver.com>
2023-02-17 19:04:05 +0900

    header구현

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,6 +1,45 @@
 import './Header.css';
+import { useState, useRef, useEffect } from 'react';
+import InputBox from './InputBox';
+import UserMenu from '../UserMenu';
 
 function Header() {
+  const [open, setOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const searchRef = useRef(null);
+  const userRef = useRef(null);
+
+  useEffect(() => {
+    function handleOutside(e) {
+      if (searchRef.current && !searchRef.current.contains(e.target)) {
+        setOpen(false);
+      }
+    }
+    document.addEventListener('mousedown', handleOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleOutside);
+    };
+  }, [searchRef]);
+
+  useEffect(() => {
+    function handleOutside(e) {
+      if (userRef.current && !userRef.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    }
+    document.addEventListener('mousedown', handleOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleOutside);
+    };
+  }, [userRef]);
+
+  const click = () => {
+    setOpen(true);
+  };
+  const click2 = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="header">
       <div className="headerContainer">
@@ -13,7 +52,7 @@ function Header() {
           </div>
           <h3>Products</h3>
         </div>
-        <div className="headerMiddle">
+        <div ref={searchRef} className="headerMiddle">
           <div className="headerSearchContainer">
             <svg
               aria-hidden="true"
@@ -24,8 +63,9 @@ function Header() {
             >
               <path d="m18 16.5-5.14-5.18h-.35a7 7 0 1 0-1.19 1.19v.35L16.5 18l1.5-1.5ZM12 7A5 5 0 1 1 2 7a5 5 0 0 1 10 0Z"></path>
             </svg>
-            <input type="text" placeholder="Search..." />
+            <input onClick={click} type="text" placeholder="Search..." />
           </div>
+          {open ? <InputBox /> : null}
         </div>
         <div className="headerRight">
           <div className="headerRightContainer">
@@ -71,16 +111,24 @@ function Header() {
                 <path d="M9 1C4.64 1 1 4.64 1 9c0 4.36 3.64 8 8 8 4.36 0 8-3.64 8-8 0-4.36-3.64-8-8-8Zm.81 12.13c-.02.71-.55 1.15-1.24 1.13-.66-.02-1.17-.49-1.15-1.2.02-.72.56-1.18 1.22-1.16.7.03 1.2.51 1.17 1.23ZM11.77 8c-.59.66-1.78 1.09-2.05 1.97a4 4 0 0 0-.09.75c0 .05-.03.16-.18.16H7.88c-.16 0-.18-.1-.18-.15.06-1.35.66-2.2 1.83-2.88.39-.29.7-.75.7-1.24.01-1.24-1.64-1.82-2.35-.72-.21.33-.18.73-.18 1.1H5.75c0-1.97 1.03-3.26 3.03-3.26 1.75 0 3.47.87 3.47 2.83 0 .57-.2 1.05-.48 1.44Z"></path>
               </svg>
             </div>
-            <div className="headerRightIcon">
-              <svg
-                aria-hidden="true"
-                className="svg-icon iconStackExchange"
-                width="18"
-                height="18"
-                viewBox="0 0 18 18"
-              >
-                <path d="M15 1H3a2 2 0 0 0-2 2v2h16V3a2 2 0 0 0-2-2ZM1 13c0 1.1.9 2 2 2h8v3l3-3h1a2 2 0 0 0 2-2v-2H1v2Zm16-7H1v4h16V6Z"></path>
-              </svg>
+            <div
+              ref={userRef}
+              onClick={click2}
+              role="presentation"
+              className="userMenuContainer"
+            >
+              <div className="headerRightIcon">
+                <svg
+                  aria-hidden="true"
+                  className="svg-icon iconStackExchange"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 18 18"
+                >
+                  <path d="M15 1H3a2 2 0 0 0-2 2v2h16V3a2 2 0 0 0-2-2ZM1 13c0 1.1.9 2 2 2h8v3l3-3h1a2 2 0 0 0 2-2v-2H1v2Zm16-7H1v4h16V6Z"></path>
+                </svg>
+              </div>
+              {isOpen ? <UserMenu /> : null}
             </div>
           </div>
         </div>
