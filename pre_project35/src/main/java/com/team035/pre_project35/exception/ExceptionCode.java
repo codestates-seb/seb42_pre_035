7ca74d3b8a67499c6933ec3d486207d7db96df09
leegri <bestest0415@gmail.com>
2023-02-22 10:37:16 +0900

    [feat]Exception , ErrorResponse 추가

---
+++ pre_project35/src/main/java/com/team035/pre_project35/exception/ExceptionCode.java
@@ -0,0 +1,21 @@
+package com.team035.pre_project35.exception;
+
+import lombok.Getter;
+
+public enum ExceptionCode {
+
+    USER_NOT_FOUND(404, "User not found"),
+
+    USER_EXISTS(409, "User exists");
+
+    @Getter
+    private int status;
+
+    @Getter
+    private String message;
+
+    ExceptionCode(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
