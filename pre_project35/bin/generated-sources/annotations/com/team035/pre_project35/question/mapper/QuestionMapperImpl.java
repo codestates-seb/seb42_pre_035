e94ecc6bba8595cb1492cf389f02fce11a98a6f3
Im Sung Eun <se2id@naver.com>
2023-03-02 18:42:59 +0900

    최종본!

---
+++ pre_project35/bin/generated-sources/annotations/com/team035/pre_project35/question/mapper/QuestionMapperImpl.java
@@ -0,0 +1,62 @@
+package com.team035.pre_project35.question.mapper;
+
+import com.team035.pre_project35.question.dto.QuestionPatchDto;
+import com.team035.pre_project35.question.dto.QuestionPostDto;
+import com.team035.pre_project35.question.dto.QuestionResponseDto;
+import com.team035.pre_project35.question.entity.Question;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-03-02T18:41:58+0900",
+    comments = "version: 1.5.3.Final, compiler: Eclipse JDT (IDE) 3.33.0.v20230213-1046, environment: Java 17.0.6 (Eclipse Adoptium)"
+)
+@Component
+public class QuestionMapperImpl implements QuestionMapper {
+
+    @Override
+    public Question questionPostDtoToQuestion(QuestionPostDto questionPostDto) {
+        if ( questionPostDto == null ) {
+            return null;
+        }
+
+        Question question = new Question();
+
+        question.setQuestionBody( questionPostDto.getQuestionBody() );
+        question.setTitle( questionPostDto.getTitle() );
+
+        return question;
+    }
+
+    @Override
+    public Question questionPatchDtoToQuestion(QuestionPatchDto questionPatchDto) {
+        if ( questionPatchDto == null ) {
+            return null;
+        }
+
+        Question question = new Question();
+
+        question.setQuestionBody( questionPatchDto.getQuestionBody() );
+        question.setQuestionId( questionPatchDto.getQuestionId() );
+        question.setTitle( questionPatchDto.getTitle() );
+
+        return question;
+    }
+
+    @Override
+    public List<QuestionResponseDto> questionToQuestionResponseDtos(List<Question> questions) {
+        if ( questions == null ) {
+            return null;
+        }
+
+        List<QuestionResponseDto> list = new ArrayList<QuestionResponseDto>( questions.size() );
+        for ( Question question : questions ) {
+            list.add( questionToquestionResponseDto( question ) );
+        }
+
+        return list;
+    }
+}
