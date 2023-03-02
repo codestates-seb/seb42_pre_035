15fb513cd52c6a270ffcf8ede67fddaabe3f81c7
Im Sung Eun <se2id@naver.com>
2023-03-02 15:29:11 +0900

    병합테스트

---
+++ client/pre_project35/src/test/java/com/team035/pre_project35/PreProject35ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.team035.pre_project35;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootTest
+class PreProject35ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-vars */
 import './Header.css';
 import { useState, useRef, useEffect, useCallback } from 'react';
 import InputBox from './InputBox';
