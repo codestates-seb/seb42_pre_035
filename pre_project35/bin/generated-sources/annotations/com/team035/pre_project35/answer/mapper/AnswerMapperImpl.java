2c462dde053ac52a2f5b19c94541401f699342bb
Im Sung Eun <se2id@naver.com>
2023-03-02 18:36:10 +0900

    최종본

---
+++ pre_project35/bin/generated-sources/annotations/com/team035/pre_project35/answer/mapper/AnswerMapperImpl.java
@@ -0,0 +1,74 @@
+package com.team035.pre_project35.answer.mapper;
+
+import com.team035.pre_project35.answer.dto.AnswerPatchDto;
+import com.team035.pre_project35.answer.dto.AnswerPostDto;
+import com.team035.pre_project35.answer.dto.AnswerResponseDto;
+import com.team035.pre_project35.answer.entity.Answer;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-03-02T18:24:31+0900",
+    comments = "version: 1.5.3.Final, compiler: Eclipse JDT (IDE) 3.33.0.v20230213-1046, environment: Java 17.0.6 (Eclipse Adoptium)"
+)
+@Component
+public class AnswerMapperImpl implements AnswerMapper {
+
+    @Override
+    public Answer answerPostDtoToAnswer(AnswerPostDto answerPostDto) {
+        if ( answerPostDto == null ) {
+            return null;
+        }
+
+        Answer answer = new Answer();
+
+        answer.setAnswerBody( answerPostDto.getAnswerBody() );
+
+        return answer;
+    }
+
+    @Override
+    public Answer answerPatchDtoToAnswer(AnswerPatchDto answerPatchDto) {
+        if ( answerPatchDto == null ) {
+            return null;
+        }
+
+        Answer answer = new Answer();
+
+        answer.setAnswerBody( answerPatchDto.getAnswerBody() );
+        answer.setAnswerId( answerPatchDto.getAnswerId() );
+
+        return answer;
+    }
+
+    @Override
+    public AnswerResponseDto answerToAnswerResponse(Answer answer) {
+        if ( answer == null ) {
+            return null;
+        }
+
+        AnswerResponseDto answerResponseDto = new AnswerResponseDto();
+
+        answerResponseDto.setAnswerBody( answer.getAnswerBody() );
+        answerResponseDto.setAnswerId( answer.getAnswerId() );
+
+        return answerResponseDto;
+    }
+
+    @Override
+    public List<AnswerResponseDto> answerToAnswerResponses(List<Answer> answers) {
+        if ( answers == null ) {
+            return null;
+        }
+
+        List<AnswerResponseDto> list = new ArrayList<AnswerResponseDto>( answers.size() );
+        for ( Answer answer : answers ) {
+            list.add( answerToAnswerResponse( answer ) );
+        }
+
+        return list;
+    }
+}
---
+++ pre_project35/bin/generated-sources/annotations/com/team035/pre_project35/user/mapper/UserMapperImpl.java
@@ -0,0 +1,79 @@
+package com.team035.pre_project35.user.mapper;
+
+import com.team035.pre_project35.user.dto.UserPatchDto;
+import com.team035.pre_project35.user.dto.UserPostDto;
+import com.team035.pre_project35.user.dto.UserResponseDto;
+import com.team035.pre_project35.user.entity.User;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-03-02T18:24:31+0900",
+    comments = "version: 1.5.3.Final, compiler: Eclipse JDT (IDE) 3.33.0.v20230213-1046, environment: Java 17.0.6 (Eclipse Adoptium)"
+)
+@Component
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public User userPostDtoToUser(UserPostDto userPostDto) {
+        if ( userPostDto == null ) {
+            return null;
+        }
+
+        User user = new User();
+
+        user.setEmail( userPostDto.getEmail() );
+        user.setName( userPostDto.getName() );
+        user.setPassword( userPostDto.getPassword() );
+
+        return user;
+    }
+
+    @Override
+    public User userPatchToUser(UserPatchDto userPatchDto) {
+        if ( userPatchDto == null ) {
+            return null;
+        }
+
+        User user = new User();
+
+        user.setName( userPatchDto.getName() );
+        user.setPassword( userPatchDto.getPassword() );
+        user.setUserId( userPatchDto.getUserId() );
+
+        return user;
+    }
+
+    @Override
+    public UserResponseDto userToUserResponse(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserResponseDto.UserResponseDtoBuilder userResponseDto = UserResponseDto.builder();
+
+        userResponseDto.email( user.getEmail() );
+        userResponseDto.name( user.getName() );
+        userResponseDto.password( user.getPassword() );
+        userResponseDto.userId( user.getUserId() );
+
+        return userResponseDto.build();
+    }
+
+    @Override
+    public List<UserResponseDto> usersToUserResponses(List<User> users) {
+        if ( users == null ) {
+            return null;
+        }
+
+        List<UserResponseDto> list = new ArrayList<UserResponseDto>( users.size() );
+        for ( User user : users ) {
+            list.add( userToUserResponse( user ) );
+        }
+
+        return list;
+    }
+}
