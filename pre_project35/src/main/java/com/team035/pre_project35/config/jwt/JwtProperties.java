ea3cbd094a456ed999a3b84d81c8f05973d1741f
Im Sung Eun <se2id@naver.com>
2023-03-02 15:32:39 +0900

    병합테스트

--- pre_project35/src/main/java/com/team035/pre_project35/config/jwt/JwtProperties.java
+++
@@ -1,8 +0,0 @@
-package com.team035.pre_project35.config.jwt;
-
-public interface JwtProperties {
-    String SECRET = "pre";
-    int EXPIRATION_TIME = 43200000;
-    String TOKEN_PREFIX = "Bearer ";
-    String HEADER_STRING = "Authorization";
-}
